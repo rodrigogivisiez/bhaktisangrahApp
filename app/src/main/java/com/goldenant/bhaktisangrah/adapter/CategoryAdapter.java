a3f8e8095be1e27f82f88c5690169e31790b7ddd
mayank <mayanklangalia@gmail.com>
2016-01-12 12:28:40 +0530

    14th Commit(Ankita)

--- app/src/main/java/com/goldenant/bhaktisangrah/adapter/CategoryAdapter.java
+++ app/src/main/java/com/goldenant/bhaktisangrah/adapter/CategoryAdapter.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
@@ -18,7 +17,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.goldenant.bhaktisangrah.MainActivity;
@@ -32,7 +30,6 @@ import com.goldenant.bhaktisangrah.model.SubCategoryModel;
 import com.squareup.picasso.Picasso;
 
 import org.apache.http.util.ByteArrayBuffer;
-import org.w3c.dom.Text;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -186,13 +183,13 @@ public class CategoryAdapter extends ArrayAdapter<HomeModel> {
                         if(mItem.get(position).getItem_id().equalsIgnoreCase(songsID.get(i))){
 
                             mViewHolder.download.setBackgroundResource(R.drawable.download_finished);
-                            mViewHolder.download.setClickable(false);
+                            mViewHolder.download.setOnClickListener(null);
                             ClickGuard.guard(mViewHolder.download);
                             break;
                         }
                     }
-
                 }
+
                 mViewHolder.download.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
